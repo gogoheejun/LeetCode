30401fd71123ae9d832e193d64b44617e74bdb80
Heejun Yang <78577071+gogoheejun@users.noreply.github.com>
2022-01-02 15:22:01 +0900

    Time: 3 ms (65.01%), Space: 43 MB (5.73%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,18 +1,21 @@
+
+import java.util.HashMap;
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
-        
-        for(int i=0; i<nums.length; i++){
-            
-            Integer diff = (Integer)(target - nums[i]);    
-            if(hash.containsKey(diff)){
-                int answer[] = {hash.get(diff), i};
-                return answer;
-            }else{
-                hash.put(nums[i], i);
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.get(diff) != null) {
+                return new int[]{i, map.get(diff)};
+                
+            } else {
+                map.put(nums[i], i);
             }
         }
-        
+
         return null;
     }
 }
